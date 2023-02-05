5eaaa5d882eedad9bb9f6bbf6e20ac4463136f42
Michel de Geofroy <micheldegeofroy@gmail.com>
2023-02-05 17:09:19 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,16 +16,6 @@ sudo apt upgrade -y
 echo " "
 echo " "
 echo "##########################################"
-echo "Set Priviliges for www-data"
-echo "##########################################"
-echo " "
-echo " "
-
-sudo usermod -a -G sudo www-data
-
-echo " "
-echo " "
-echo "##########################################"
 echo "Get Bot Token and Admin Chat ID"
 echo "##########################################"
 echo " "
